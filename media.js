1568a7460f2f43504156c261dc2fb8a40c628ccf
AUSTREA <105995511+AUSTREA@users.noreply.github.com>
2026-04-05 12:53:02 -0600

    Update media.js

--- media.js
+++ media.js
@@ -1,13 +1,1 @@
-// funciones/media.js
-
-function volverPanel() {
-  window.location.href = "index.html";
-}
-
-function irCarta() {
-  window.location.href = "carta.html";
-}
-
-function cargarXML() {
-  alert("Función de carga XML pendiente de implementar.");
-}
+// funciones/media.j
