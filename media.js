a7fd0f1b4fe2c46274dfc04413639da7bf3c0a9a
AUSTREA <105995511+AUSTREA@users.noreply.github.com>
2026-04-05 14:47:02 -0600

    Update media.js
    no hace nada

--- media.js
+++ media.js
@@ -1,1 +1,13 @@
-// funciones/media.j
+// funciones/media.js
+
+function volverPanel() {
+  window.location.href = "index.html";
+}
+
+function irCarta() {
+  window.location.href = "carta.html";
+}
+
+function cargarXML() {
+  alert("Función de carga XML pendiente de implementar.");
+}
